63c8e5df5d5bc15b586ec0dea51aae31e2da7714
OCurdy <olivier_curdy@hotmail.com>
2026-04-01 13:00:38 +0200

    fix: use gthread worker to avoid JVM fork issue with saxonche

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,7 @@
 import multiprocessing
 
-workers = int(multiprocessing.cpu_count() * 2) + 1
-threads = int(multiprocessing.cpu_count() * 2)
+# saxonche embeds a JVM which is not fork-safe.
+# Using gthread worker (thread-based) avoids forking after JVM init.
+worker_class = "gthread"
+workers = 1
+threads = int(multiprocessing.cpu_count() * 2) + 1
